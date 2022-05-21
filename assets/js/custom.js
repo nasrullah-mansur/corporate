06aeb44b87e7612b9c35b4630403bb1244c3e694
nasrullahmansur <nasrullah.cit.bd@gmail.com>
2022-05-21 23:34:22 +0600

    project

---
+++ assets/js/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    "use strict";
+
+
+    $('.header .content .mobile-toggler').on('click', function() {
+        $('.header .content .list').slideToggle();
+    })
+});
